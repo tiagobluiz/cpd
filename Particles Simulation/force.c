ab7806407ee8527607b2db01398d08dbc348688f
Diogo Dias <dpdias97@gmail.com>
2019-02-28 19:53:59 +0000

    Cont. of force applied to part

--- Particles Simulation/force.c
+++ Particles Simulation/force.c
@@ -1,17 +1,67 @@
 #include "Header.h"
+#define MINIMUM_DISTANCE 0,01
 
+//struct that represents the force applied to a particle
 typedef struct {
-    double force;
-    double angle;
+    double force;       // double which contains the force applied to the particle in N
+    double angle;       //double which contains the angle of the force where the X axis is the base
 } force_vector;
 
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!! if the particle and the center of mass are to close, the force should be 0
 double computeMagnitudeForce(particle_t* a, cell * b) {
-    return G * ( a->m * b->m ) / ( exp2 ( sqrt ( exp2 ( a->x - b->x ) + exp2 ( a->y - b->y ))));
+    double distance_a_b = sqrt ( exp2 ( a->x - b->x ) + exp2 ( a->y - b->y ));
+    if(distance_a_b < MINIMUM_DISTANCE)
+        return 0;
+    return G * ( a->m * b->m ) / ( exp2 (distance_a_b));
 }
 
+/**
+ * Function that gets the cell with the coordinates (unbound_row, unbound_column). Those coordinates will always represent
+ * a cell because the space is wrapped, that is, if a particle exits through the side of the space it enters on the corresponding
+ * position on the opposite side of the space.
+ * @param unbound_row       indice in X axis
+ * @param unbound_column    indice in Yaxis
+ * @param cells             matrix with the cells to search
+ * @param return_cell       cell which we were trying to get
+ * @param side              number of cells in each side
+ * @param cell_dimension    dimension of each cell
+ */
+void getCell(int unbound_row, int unbound_column, cell ** cells, cell * return_cell int side, double cell_dimension){
+    int bounded_row = unbounded_row%side;
+    int bounded_column = unbounded_column%side;
+
+    if((unbounded_row >= side && unbounded_column >= side) || unbounded_row < 0 && unbounded_column < 0){
+        return_cell->x = unbounded_column*cell_dimension + cells[bounded_row][bounded_column].x;
+        return_cell->y = unbounded_row*cell_dimension + cells[bounded_row][bounded_column].y;
+        return_cell->m = cells[bounded_row][bounded_column].m;
+    }
+    else if(unbounded_row >= side || unbounded_row < 0){
+        return_cell->y = unbounded_row*cell_dimension + cells[bounded_row][bounded_column].y;
+        return_cell->x = cells[bounded_row][bounded_column].x;
+        return_cell->m = cells[bounded_row][bounded_column].m;
+    }
+    else if(unbounded_column >= side || unbounded_column < 0){
+        return_cell->x = unbounded_column*cell_dimension + cells[bounded_row][bounded_column].x;
+        return_cell->y = cells[bounded_row][bounded_column].y;
+        return_cell->m = cells[bounded_row][bounded_column].m;
+    }
+    else {
+        return_cell = &cells[bounded_row][bounded_column];
+    }
+}
 
 // we can create DUMMY CELLS around the grid!!
-void calculateForce(particle_t * particles, int particlesLength, cell ** cells, int cellsLength, force_vector * forceVector, int side, double dimension_cell){
+/**
+ * Function that calculates the force being applied to all particles
+ * @param particles         list with the particles
+ * @param particlesLength   number of particles in the list
+ * @param cells             matrix that represents the cells
+ * @param forceVector       list to return the resultant force being applied to each particle
+ * @param side              number of cells in each side
+ * @param cell_dimension    dimension of each cell
+ */
+void calculateForce(particle_t * particles, int particlesLength, cell ** cells, force_vector * forceVector, int side, double cell_dimension){
 
     //iterate all cells
     for(int i = 0; i < particlesLength; i++ ){
@@ -27,22 +77,17 @@ void calculateForce(particle_t * particles, int particlesLength, cell ** cells, 
         // iterate all the 9 mass cells around the particle
         for(int row = -1; row < 2; row++){
             for(int column = -1; column < 2; column++){
-                i_row = row + cell_x;
-                i_column = column + cell_y;
-
-                if(i_row > side){
-
-                }
-                //get respective cell
-                cell * p_cell = &cells[()%side][()%side];
+                //get the neighbor cell in the coordinates (row, column)
+                cell neighbor_cell;
+                getCell(row + cell_x, column + cell_y, cells, &neighbor_cell, side, cell_dimension);
 
                 //compute angle
-                double delta_x = p_cell.x - particles[i].x;
-                double delta_y = p_cell.y - particles[i].y;
+                double delta_x = neighbor_cell.x - particles[i].x;
+                double delta_y = neighbor_cell.y - particles[i].y;
                 double vector_angle = atan2(delta_y, delta_x);
 
                 //compute force !!!!!!!!!!!!!!!! verificar se é assim
-                double force = computeMagnitudeForce(&particles[i], p_cell);
+                double force = computeMagnitudeForce(&particles[i], &neighbor_cell);
 
                 //compute resultant force
                 // it's in radians or degrees????????????
@@ -50,11 +95,10 @@ void calculateForce(particle_t * particles, int particlesLength, cell ** cells, 
                 Fy += force*((M_PI/2) - vector_angle);
             }
         }
-        double resultant_force = sqrt( Fx*Fx + Fy*Fy );
-        double resultant_force_angle = acos(Fx/resultant_force);
 
-        forceVector[i].force = resultant_force;
-        forceVector[i].angle = resultant_force_angle;
+        //calculate the resultant force and the respective angle
+        forceVector[i].force = sqrt( Fx*Fx + Fy*Fy );
+        forceVector[i].angle = acos(Fx/resultant_force);
     }
 }
 
